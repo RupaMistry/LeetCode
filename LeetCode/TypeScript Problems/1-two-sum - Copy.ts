be8dc459023e4312caa7a8a4a2ccbaeb80cd448d
RupaMistry <mistryrupa7@gmail.com>
2026-01-14 11:53:43 +0400

    duplciate

---
+++ LeetCode/TypeScript Problems/1-two-sum - Copy.ts
@@ -0,0 +1,21 @@
+﻿function twoSum(nums: number[], target: number): number[] {
+    let result: number[] = [-1, -1];
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                result[0] = i;
+                result[1] = j;
+                break;
+            }
+        }
+
+        if (result[0] !== -1) {
+            break;
+        }
+    }
+
+    return result;
+};
+
+//  Typescript solution for LeetCode 2. Add Two Numbers
